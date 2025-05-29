bc856de12ebdeddc13303ed036e4070b8ea4b79c
Alexander Vikenfalk <alexander.wikenfalk@gmail.com>
2025-05-29 20:47:15 +0200

    feat: add Card and Header components

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,44 +6,19 @@ interface CardProps {
 
 export const Card = ({ movie }: CardProps) => {
     return (
-        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
-            <a href="#">
+        <div key={movie.id} className="group cursor-pointer">
+            <div className="overflow-hidden rounded-sm">
                 <img
-                    className="rounded-t-lg"
                     src={movie.poster_path}
                     alt={movie.title}
+                    className="w-full h-auto object-cover transition-transform duration-300 group-hover:scale-105"
                 />
-            </a>
-            <div className="p-5">
-                <a href="#">
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                        {movie.title}
-                    </h5>
-                </a>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                    {movie.overview}
+            </div>
+            <div className="mt-2 text-center text-sm text-white">
+                <p className="font-semibold truncate">{movie.title}</p>
+                <p className="text-gray-400 text-xs">
+                    {new Date(movie.release_date).getFullYear()}
                 </p>
-                <a
-                    href="#"
-                    className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                    Read more
-                    <svg
-                        className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 14 10"
-                    >
-                        <path
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M1 5h12m0 0L9 1m4 4L9 9"
-                        />
-                    </svg>
-                </a>
             </div>
         </div>
     )
