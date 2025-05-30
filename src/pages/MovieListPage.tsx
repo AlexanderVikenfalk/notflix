9756b8fc25003986c379925dd25030234930fb74
Alexander Vikenfalk <alexander.wikenfalk@gmail.com>
2025-05-30 17:08:11 +0200

    feat: add temp movie list page

--- src/pages/MovieListPage.tsx
+++ src/pages/MovieListPage.tsx
@@ -2,23 +2,23 @@ import { useFetch } from '../hooks/useFetch'
 import { useTitle } from '../hooks/useTitle'
 import { Card } from '../components/Card'
 
-import type { MovieSearchResult } from '@/types/interfaces'
+import type { MovieSearchResponse } from '@/types/interfaces'
 
 interface MovieListProps {
     title: string
     apiPath: string
 }
 
-export const MovieListPage = ({ apiPath, title }: MovieListProps) => {
-    const { data } = useFetch<MovieSearchResult[]>(apiPath)
-    const movies = data ?? []
+export const MovieListPage = ({ apiPath }: MovieListProps) => {
+    const { data } = useFetch<MovieSearchResponse>(apiPath)
+    const movies = data?.results ?? []
 
-    useTitle(title)
+    useTitle('Popular')
 
     return (
         <main>
             <section className="max-w-7xl mx-auto py-7">
-                <div className="flex justify-start flex-wrap other:justify-evenly">
+                <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 px-4">
                     {movies.map((movie) => (
                         <Card key={movie.id} movie={movie} />
                     ))}
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,6 @@
 export { MovieListPage } from './MovieListPage.tsx'
 export { MovieDetailsPage } from './MovieDetailsPage'
 export { FavoritesPage } from './FavoritesPage'
+export { SearchResultsPage } from './SearchResultsPage'
 
 export { NotFoundPage } from './NotFoundPage'
