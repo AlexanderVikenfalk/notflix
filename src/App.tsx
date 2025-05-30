3dfd02866f84af7ca569d702717496f66867a848
Alexander Vikenfalk <alexander.wikenfalk@gmail.com>
2025-05-30 21:10:05 +0200

    feat: add loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import { Outlet } from 'react-router-dom'
 import { Header, Footer } from './components'
+import { Outlet } from 'react-router-dom'
+import { Suspense } from 'react'
+import Loader from '@/components/Loader'
 
 function App() {
     return (
-        <div className="bg-blue-950 h-screen">
-                <Header />
-                <main>
-                    <Outlet />
-                </main>
-                <Footer />
+        <div className="bg-blue-950 min-h-screen">
+            <Header />
+            <main>
+                <Suspense fallback={<Loader />}>
+                <Outlet />
+                </Suspense>
+            </main>
+            <Footer />
         </div>
     )
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { Footer } from './Footer'
 export { Header } from './Header'
+export {  default as Loader } from './Loader'
