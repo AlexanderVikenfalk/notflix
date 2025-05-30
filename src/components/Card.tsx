6b0ea829aae62b020e4fce632ce92bb14f06350c
Alexander Vikenfalk <alexander.wikenfalk@gmail.com>
2025-05-30 17:03:53 +0200

    refactor: add movie Link to card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,5 @@
 import type { MovieSearchResult } from '@/types/interfaces'
+import { Link } from 'react-router-dom'
 
 interface CardProps {
     movie: MovieSearchResult
@@ -7,19 +8,22 @@ interface CardProps {
 export const Card = ({ movie }: CardProps) => {
     return (
         <div key={movie.id} className="group cursor-pointer">
-            <div className="overflow-hidden rounded-sm">
-                <img
-                    src={movie.poster_path}
-                    alt={movie.title}
-                    className="w-full h-auto object-cover transition-transform duration-300 group-hover:scale-105"
-                />
-            </div>
-            <div className="mt-2 text-center text-sm text-white">
-                <p className="font-semibold truncate">{movie.title}</p>
-                <p className="text-gray-400 text-xs">
-                    {new Date(movie.release_date).getFullYear()}
-                </p>
-            </div>
+            <Link to={`/movie/${movie.id}`}>
+                <div className="overflow-hidden rounded-sm relative">
+                    <img
+                        src={movie.poster_path}
+                        alt={movie.title}
+                        className="w-full h-auto object-cover transition-transform duration-[400ms] ease-in-out scale-100 group-hover:scale-105"
+                    />
+                    <div className="absolute inset-0 border-2 border-transparent group-hover:border-white pointer-events-none" />
+                </div>
+                <div className="mt-2 text-center text-sm text-white">
+                    <p className="font-semibold truncate">{movie.title}</p>
+                    <p className="text-gray-400 text-xs">
+                        {new Date(movie.release_date).getFullYear()}
+                    </p>
+                </div>
+            </Link>
         </div>
     )
 }
