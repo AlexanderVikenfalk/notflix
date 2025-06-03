065a0c01b19de8cd37131f7027b4937eea6a68ed
Alexander Vikenfalk <alexander.wikenfalk@gmail.com>
2025-06-03 07:23:05 +0200

    refactor: add generics

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,9 +1,18 @@
 import axiosInstance from '@/utils/axiosInstance'
+import type { AxiosResponse } from 'axios'
+import type {
+    MovieSearchResponse,
+    MovieDetails,
+} from '@/types/interfaces'
 
-export const getMovies = (page: number) =>
+export const getMovies = (page: number): Promise<AxiosResponse<MovieSearchResponse>> =>
     axiosInstance.get(`/movies?page=${page}`)
 
-export const getMovieById = (id: string) => axiosInstance.get(`/movie/${id}`)
+export const getMovieById = (id: string): Promise<AxiosResponse<MovieDetails>> =>
+    axiosInstance.get(`/movie/${id}`)
 
-export const searchMovies = (query: string, page = 1) =>
+export const searchMovies = (
+    query: string,
+    page = 1
+): Promise<AxiosResponse<MovieSearchResponse>> =>
     axiosInstance.get(`/search?q=${query}&page=${page}`)
