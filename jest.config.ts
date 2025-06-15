87bc7d224851aa6ccd0e04a60ffca3c18030d26b
Alexander Vikenfalk <alexander.wikenfalk@gmail.com>
2025-06-15 15:32:59 +0200

    refactor: simplify TS configs and remove duplicate rules

--- jest.config.ts
+++ jest.config.ts
@@ -7,7 +7,6 @@ export default {
         '^@/(.*)$': '<rootDir>/src/$1',
         '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
         "^.+\\.(svg)$": "<rootDir>/src/__mocks__/svgMock.tsx",
-        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/src/__mocks__/fileMock.js'
     },
     testPathIgnorePatterns: [
         '<rootDir>/playwrightTests/',
